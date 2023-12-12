11e9b2938b901d863a0776fd293b5f67f0d93cd8
Kunal Verma <jikunalvermabbbb@gmail.com>
2023-12-12 21:52:16 +0530

    Project Management commit

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState('');
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    if (enteredTask.trim() === '') {
+      return;
+    }
+    onAdd(enteredTask);
+    setEnteredTask('');
+  }
+
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+      onClick={handleClick}>
+        Add Task
+      </button>
+    </div>
+  );
+}
